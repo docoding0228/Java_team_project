97e376391dcc85262934f3c982a53570c0a64924
yoon <pyh051920@gmail.com>
2024-05-02 21:10:05 +0900

    사람만드는 기능 추가1

--- Team_project/src/Score.java
+++ Team_project/src/Score.java
@@ -1,19 +1,23 @@
 public class Score {
     private final int subject;
     private final int StudentId;
-    private final int score_rank;
+    private final int scoreRank;
     private final int episode;
     private final char rank;
-    public Score(int StudentId, int subject,  int episode, int score_rank){
+
+    // 여기에 들어올것 : subject, episode(회차), 점수
+    public Score(int StudentId, int subject,  int episode, int scoreRank){
         this.subject = subject;
-        this.StudentId = StudentId;
-        this.score_rank = score_rank;
+        this.StudentId = StudentId;  // 필요 없음 , 키값,...
+        this.scoreRank = scoreRank;
         this.episode = episode;
+        // 이거는 따로 setRank() 메서드 만들어서 하기
+        // public void setRank(scoreRank){this.rank =...}
         if(subject <=5){
-            switch(score_rank/10){
+            switch(scoreRank/10){
                 case 10:
                 case 9:
-                    if(score_rank >= 95)
+                    if(scoreRank >= 95)
                         rank = 'A';
                     else
                         rank = 'B';
@@ -32,7 +36,7 @@ public class Score {
                     break;
             }
         }else {
-            switch(score_rank/10){
+            switch(scoreRank/10){
                 case 100:
                 case 9:
                     rank = 'A';
