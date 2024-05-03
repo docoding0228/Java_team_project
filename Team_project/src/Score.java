a5e68f61e39bd0404a3d36f36dcf0e602d992823
yoon <pyh051920@gmail.com>
2024-05-03 17:25:18 +0900

    사람 클래스 추가1

--- Team_project/src/Score.java
+++ Team_project/src/Score.java
@@ -1,14 +1,15 @@
+import java.util.ArrayList;
+
 public class Score {
     private final int subject;
-    private final int StudentId;
     private final int scoreRank;
     private final int episode;
     private final char rank;
+    ArrayList<ArrayList<ArrayList<Integer>>> scores = new ArrayList<>();
 
     // 여기에 들어올것 : subject, episode(회차), 점수
-    public Score(int StudentId, int subject,  int episode, int scoreRank){
+    public Score(int subject,  int episode, int scoreRank){
         this.subject = subject;
-        this.StudentId = StudentId;  // 필요 없음 , 키값,...
         this.scoreRank = scoreRank;
         this.episode = episode;
         // 이거는 따로 setRank() 메서드 만들어서 하기
@@ -18,40 +19,40 @@ public class Score {
                 case 10:
                 case 9:
                     if(scoreRank >= 95)
-                        rank = 'A';
+                        this.rank = 'A';
                     else
-                        rank = 'B';
+                        this.rank = 'B';
                     break;
                 case 8:
-                    rank = 'C';
+                    this.rank = 'C';
                     break;
                 case 7:
-                    rank = 'D';
+                    this.rank = 'D';
                     break;
                 case 6:
-                    rank = 'F';
+                    this.rank = 'F';
                     break;
                 default:
-                    rank = 'N';
+                    this.rank = 'N';
                     break;
             }
         }else {
             switch(scoreRank/10){
                 case 100:
                 case 9:
-                    rank = 'A';
+                    this.rank = 'A';
                     break;
                 case 8:
-                    rank = 'C';
+                    this.rank = 'C';
                     break;
                 case 7:
-                    rank = 'D';
+                    this.rank = 'D';
                     break;
                 case 6:
-                    rank = 'F';
+                    this.rank = 'F';
                     break;
                 default:
-                    rank = 'N';
+                    this.rank = 'N';
                     break;
             }
         }
