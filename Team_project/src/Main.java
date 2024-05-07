edad24a35325741f93383f8535821fd50c42ac13
yoon <pyh051920@gmail.com>
2024-05-07 11:40:18 +0900

    점수관리 수정 1 : 스코어 클래스에 점수입력 기능 추가

--- Team_project/src/Main.java
+++ Team_project/src/Main.java
@@ -10,13 +10,6 @@ public class Main {
         Map<String, Student> studentMap = new HashMap<>();
         do {
             try {
-//                System.out.println("==================================");
-//                System.out.println("점수 관리 실행 중...");
-//                System.out.println("1. 수강생의 과목별 시험 회차 및 점수 등록");
-//                System.out.println("2. 수강생의 과목별 회차 점수 수정");
-//                System.out.println("3. 수강생의 특정 과목 회차별 등급 조회");
-//                System.out.println("4. 메인 화면 이동");
-//                System.out.print(" 관리 항목을 선택하세요... ");
                 System.out.println("1. 수강생 정보 입력,  2. 회차 및 점수 등록 : ,  3. 수강생 정보 검색 : ");
                 int selectlist = sc.nextInt(); // 내부 메뉴 선택
 
@@ -63,22 +56,9 @@ public class Main {
                         }
                     }
                 }
-//                else if (selectlist == 3) { // 특정 과목 회차별 등급 조회
-//                    System.out.println("해당 기능은 구현 중입니다!");
-//                } else if (selectlist == 4) { // 메인 화면 이동
-//                    System.out.println("메인 화면으로 돌아갑니다.");
-//                }
                 else {
                     System.out.println("유효하지 않은 선택입니다.");
                 }
-
-                // nameMap 프린트
-//                System.out.println("현재 등록된 사람들:");
-//                for (Map.Entry<String, Student> entry : studentMap.entrySet()) {
-//                    System.out.println("{" + entry.getKey() + ", " + entry.getValue() + "}");
-//                }
-
-
 
                 System.out.println("종료하시려면 'exit'을 입력하세요.");
             } catch (Exception e) {
