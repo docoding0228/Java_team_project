ef6bf47a4e424110a74da5bfc4d56fa3b9caa123
yoon <pyh051920@gmail.com>
2024-05-08 16:36:31 +0900

    수강생 정보 수정 작업중1

--- Team_project/src/mypk/Student.java
+++ Team_project/src/mypk/Student.java
@@ -45,22 +45,24 @@ public class Student {
             System.out.println("==================================");
             System.out.println("수강생 관리 실행 중...");
             System.out.println("1. 수강생 등록");
-            System.out.println("2. 수강생 목록 조회");
-            System.out.println("3. 수강생 과목 등록");
-            System.out.println("4. 수강생 과목 조회");
-            System.out.println("5. 수강생 과목 수정");
-            System.out.println("6. 메인 화면 이동");
+            System.out.println("2. 수강생 정보 수정");
+            System.out.println("3. 수강생 목록 조회");
+            System.out.println("4. 수강생 과목 등록");
+            System.out.println("5. 수강생 과목 조회");
+            System.out.println("6. 수강생 과목 수정");
+            System.out.println("7. 메인 화면 이동");
             System.out.print("관리 항목을 선택하세요... ");
 
             int choice = sc.nextInt();
 
             switch (choice) {
                 case 1 -> Student.registerStudent(); // 수강생 등록
-                case 2 -> Student.listStudents();  // 수강생 목록 조회
-                case 3 -> Subject.manageSubjects(); // 수강생 과목 추가
-                case 4 -> Subject.subjectCheck();//수강생 과목 조회
-                case 5 -> Subject.subjectEdit();  //수강생 과목 수정
-                case 6 -> running = false; // 메인 화면으로 돌아가기
+                case 2 -> Student.editStudentNameStatus();  // 수강생 목록 조회
+                case 3 -> Student.listStudents();  // 수강생 목록 조회
+                case 4 -> Subject.manageSubjects(); // 수강생 과목 추가
+                case 5 -> Subject.subjectCheck();//수강생 과목 조회
+                case 6 -> Subject.subjectEdit();  //수강생 과목 수정
+                case 7 -> running = false; // 메인 화면으로 돌아가기
                 default -> {
                     System.out.println("잘못된 입력입니다. 메인 화면으로 돌아갑니다.");
                     running = false;
@@ -77,5 +79,32 @@ public class Student {
         System.out.print("수강생 ID를 입력하세요: ");
         String studentId = sc.next();
         return studentId;
+    }
+
+    // 학생의 이름, 상태 정보를 입력받아 수정한다.
+    public static void editStudentNameStatus(){
+        String studentId = pushID();
+
+        // 중복된 ID가 있는지 확인
+        if (!studentMap.containsKey(studentId)) {
+            System.out.println("존재하지 않는 ID 입니다.");
+        } else {
+            System.out.println("수정하고 싶은 내용은 선택하세요.");
+            System.out.println("1. 수강생 이름 수정");
+            System.out.println("2. 수강생 상태 수정");
+            System.out.println("3. 이전으로 돌아가기");
+             int choice = sc.nextInt();
+            if(choice == 1) {
+                System.out.print("새로운 이름을 입력하세요: ");
+                String studentName = sc.next();
+                studentMap.put(studentId, studentName);
+                System.out.println("수강생 이름 수정 성공!");
+                System.out.println("수강생 고유번호: " + studentId + ", 이름: " + studentName);
+            } else if(choice == 2) {
+                System.out.println("수정");
+            } else{
+                System.out.println("이전 화면으로 돌아갑니다.");
+            }
+        }
     }
 }
